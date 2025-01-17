eda9c9feedf7030baab79dfad8f80240f3a73b8e
CoolerProRB <118531502+CoolerProRB@users.noreply.github.com>
2025-01-17 18:52:39 +0800

    Bug fix

--- src/main/java/com/coolerpromc/moregears/armor/custom/TitaniumArmor.java
+++ src/main/java/com/coolerpromc/moregears/armor/custom/TitaniumArmor.java
@@ -5,6 +5,6 @@ import net.minecraft.item.ArmorItem;
 
 public class TitaniumArmor extends ArmorItem {
     public TitaniumArmor(Type type) {
-        super(MGArmorMaterials.STEEL_ARMOR_MATERIAL, type, new Settings().fireproof().maxDamage(type.getMaxDamage(45)));
+        super(MGArmorMaterials.TITANIUM_ARMOR_MATERIAL, type, new Settings().fireproof().maxDamage(type.getMaxDamage(45)));
     }
 }
