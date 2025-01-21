386b149e9b7356c8782518552b34e8d0534b0f3c
CoolerProRB <118531502+CoolerProRB@users.noreply.github.com>
2025-01-21 23:32:27 +0800

    Changed to record, removed optional method for codec

--- src/main/java/com/coolerpromc/moregears/recipe/custom/SizedIngredient.java
+++ src/main/java/com/coolerpromc/moregears/recipe/custom/SizedIngredient.java
@@ -1,7 +1,6 @@
 package com.coolerpromc.moregears.recipe.custom;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
@@ -11,14 +10,9 @@ import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.dynamic.Codecs;
 
-import java.util.Objects;
-import java.util.Optional;
-
-public final class SizedIngredient {
-    public static final Codec<SizedIngredient> NESTED_CODEC = RecordCodecBuilder.create((instance) -> instance.group(Ingredient.CODEC.fieldOf("ingredient").forGetter(SizedIngredient::ingredient), optionalFieldAlwaysWrite(Codecs.POSITIVE_INT, "count", 1).forGetter(SizedIngredient::count)).apply(instance, SizedIngredient::new));
+public record SizedIngredient(Ingredient ingredient, int count) {
+    public static final Codec<SizedIngredient> NESTED_CODEC = RecordCodecBuilder.create((instance) -> instance.group(Ingredient.CODEC.fieldOf("ingredient").forGetter(SizedIngredient::ingredient), Codecs.POSITIVE_INT.fieldOf("count").forGetter(SizedIngredient::count)).apply(instance, SizedIngredient::new));
     public static final PacketCodec<RegistryByteBuf, SizedIngredient> STREAM_CODEC;
-    private final Ingredient ingredient;
-    private final int count;
 
     public static SizedIngredient of(ItemConvertible item, int count) {
         return new SizedIngredient(Ingredient.ofItem(item), count);
@@ -33,14 +27,6 @@ public final class SizedIngredient {
         }
     }
 
-    public Ingredient ingredient() {
-        return this.ingredient;
-    }
-
-    public int count() {
-        return this.count;
-    }
-
     public boolean test(ItemStack stack) {
         return this.ingredient.test(stack) && stack.getCount() >= this.count;
     }
@@ -51,13 +37,9 @@ public final class SizedIngredient {
         } else if (!(o instanceof SizedIngredient)) {
             return false;
         } else {
-            SizedIngredient other = (SizedIngredient)o;
+            SizedIngredient other = (SizedIngredient) o;
             return this.count == other.count && this.ingredient.equals(other.ingredient);
         }
-    }
-
-    public int hashCode() {
-        return Objects.hash(this.ingredient, this.count);
     }
 
     public String toString() {
@@ -66,9 +48,5 @@ public final class SizedIngredient {
 
     static {
         STREAM_CODEC = PacketCodec.tuple(Ingredient.PACKET_CODEC, SizedIngredient::ingredient, PacketCodecs.VAR_INT, SizedIngredient::count, SizedIngredient::new);
-    }
-
-    public static <T> MapCodec<T> optionalFieldAlwaysWrite(Codec<T> codec, String name, T defaultValue) {
-        return codec.optionalFieldOf(name).xmap((o) -> o.orElse(defaultValue), Optional::of);
     }
 }
