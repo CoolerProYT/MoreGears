9b56721b9ce258e497c5b9f5a923fb26e72c5161
CoolerProYT <veronlam1818@gmail.com>
2025-04-21 14:50:50 +0800

    v1.0.8-Fixed Crash on Dedicated Server, Increased Bronze Tools underwater destroy speed

--- src/main/java/com/coolerpromc/moregears/tool/bronze/BronzeShovel.java
+++ src/main/java/com/coolerpromc/moregears/tool/bronze/BronzeShovel.java
@@ -2,6 +2,7 @@ package com.coolerpromc.moregears.tool.bronze;
 
 import com.coolerpromc.moregears.util.MGTooltip;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
@@ -17,14 +18,20 @@ public class BronzeShovel extends ShovelItem {
 
     @Override
     public float getDestroySpeed(ItemStack stack, BlockState state) {
+        boolean underwaterSpeed = stack.getOrCreateTag().getBoolean("UnderwaterSpeed");
         float originalSpeed = super.getDestroySpeed(stack, state);
+        return underwaterSpeed ? originalSpeed * 3f : originalSpeed;
+    }
 
-        Player player = Minecraft.getInstance().player;
-        if(player != null && player.isInWater()){
-            return originalSpeed * 2f;
+    @Override
+    public boolean onBlockStartBreak(ItemStack itemstack, BlockPos pos, Player player) {
+        if (player != null && player.isInWater()) {
+            player.getMainHandItem().getOrCreateTag().putBoolean("UnderwaterSpeed", true);
+        } else {
+            assert player != null;
+            player.getMainHandItem().getOrCreateTag().remove("UnderwaterSpeed");
         }
-
-        return originalSpeed;
+        return super.onBlockStartBreak(itemstack, pos, player);
     }
 
     @Override
