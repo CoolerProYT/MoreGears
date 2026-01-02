df6a6a1e858b2e44f46dab929d7a14e2075167a9
CoolerProYT <veronlam1818@gmail.com>
2026-01-02 15:51:44 +0800

    2.1.0

--- src/main/java/com/coolerpromc/moregears/MoreGears.java
+++ src/main/java/com/coolerpromc/moregears/MoreGears.java
@@ -11,10 +11,8 @@ import com.coolerpromc.moregears.recipe.MGRecipes;
 import com.coolerpromc.moregears.screen.MGMenuTypes;
 import com.coolerpromc.moregears.worldgen.MGWorldGeneration;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.recipe.v1.sync.RecipeSynchronization;
 import net.minecraft.block.DispenserBlock;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MoreGears implements ModInitializer {
 	public static final String MODID = "moregears";
@@ -39,5 +37,7 @@ public class MoreGears implements ModInitializer {
 		DispenserBlock.registerProjectileBehavior(MGItems.RUBY_ARROW);
 		DispenserBlock.registerProjectileBehavior(MGItems.TITANIUM_ARROW);
 		DispenserBlock.registerProjectileBehavior(MGItems.ENDERITE_ARROW);
+
+        RecipeSynchronization.synchronizeRecipeSerializer(MGRecipes.ALLOY_SMELTING_SERIALIZER);
 	}
 }
--- src/main/java/com/coolerpromc/moregears/compat/jei/JEIPlugin.java
+++ src/main/java/com/coolerpromc/moregears/compat/jei/JEIPlugin.java
@@ -1,24 +1,21 @@
-/*
 package com.coolerpromc.moregears.compat.jei;
 
 import com.coolerpromc.moregears.MoreGears;
+import com.coolerpromc.moregears.MoreGearsClient;
 import com.coolerpromc.moregears.compat.jei.category.AlloySmeltingCategory;
 import com.coolerpromc.moregears.recipe.AlloySmeltingRecipe;
 import com.coolerpromc.moregears.recipe.MGRecipes;
 import com.coolerpromc.moregears.screen.AlloySmelterScreen;
 import mezz.jei.api.IModPlugin;
-import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.recipe.RecipeEntry;
-import net.minecraft.recipe.RecipeManager;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@JeiPlugin
 public class JEIPlugin implements IModPlugin {
     @Override
     public Identifier getPluginUid() {
@@ -32,9 +29,7 @@ public class JEIPlugin implements IModPlugin {
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        RecipeManager recipeManager = MinecraftClient.getInstance().world.getRecipeManager();
-
-        List<RecipeEntry<AlloySmeltingRecipe>> meltingRecipes = recipeManager.listAllOfType(MGRecipes.ALLOY_SMELTING_TYPE);
+        List<RecipeEntry<AlloySmeltingRecipe>> meltingRecipes = new ArrayList<>(MoreGearsClient.recipeMap.getAll(MGRecipes.ALLOY_SMELTING_TYPE));
         List<AlloySmeltingRecipe> meltingRecipeList = meltingRecipes.stream().map(RecipeEntry::value).toList();
 
         registration.addRecipes(AlloySmeltingCategory.ALLOY_SMELTING_TYPE, meltingRecipeList);
@@ -45,4 +40,3 @@ public class JEIPlugin implements IModPlugin {
         registration.addRecipeClickArea(AlloySmelterScreen.class, 100, 31, 18, 23, AlloySmeltingCategory.ALLOY_SMELTING_TYPE);
     }
 }
-*/
